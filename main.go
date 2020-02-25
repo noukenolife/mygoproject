75f256a31d6046e9386c292a2b4f06c532745003
j-yamamoto <j-yamamoto@onu.edu>
2020-02-26 00:32:19 +0900

    Use port env value when specified

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/noukenolife/mygoproject/application/http"
 )
 
 func main() {
+	PORT := os.Getenv("PORT")
 	r := gin.Default()
 	http.InitRouter(r)
-	r.Run("127.0.0.1:8080")
+
+	if PORT != "" {
+		r.Run(fmt.Sprintf(":%s", PORT))
+	} else {
+		r.Run("127.0.0.1:8080")
+	}
 }
